c7a510b88602c5695c2f8c48b2b0998fc53e85b3
CMpachas <mauriciojcs19@hotmail.com>
2024-10-04 17:51:33 -0600

    EXPLICACIÓN

---
+++ hellogit3.py
@@ -0,0 +1,4 @@
+print("Este proyecto consiste en contarte una historia solo usando print")
+print("La historia consiste de un pajaro que queria volar pero no podia porque era pequeño")
+print("asi que su mama le enseño a volar y despues de varios intentos lo logro")
+print("fin")
